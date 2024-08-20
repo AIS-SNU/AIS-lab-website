d3236bced51338a88befcaba3fc9a08f116c9c1b
KanghyunChoi <jwchlrkdgus@gmail.com>
2024-08-20 20:34:55 +0900

    fix broken github address bug

--- _plugins/hash.rb
+++ _plugins/hash.rb
@@ -8,15 +8,26 @@ module Jekyll
         return hash
       end
       defaults.each do |key, value|
+        # Debugging: Print the current key and value
+        # puts "Processing key: #{key}, value: #{value}"
+        
         # substitute main string into default string and set main item
         if value.is_a?(String) and value.include?"$VALUE"
           if hash[key].is_a?(String)
-            hash[key] = value.sub"$VALUE", hash[key]
+            new_value = value.sub"$VALUE", hash[key]
+            # puts "#{hash[key]}, #{value.sub("$VALUE", "")} "
+            unless hash[key].include?(value.sub("$VALUE", ""))
+              hash[key] = new_value
+            end
+            # Debugging: Print the updated hash value
+            # puts "Updated hash[#{key}]: #{hash[key]}"
           end
         # set main item to default item if not defined
         else
           if hash[key] == nil or !hash.key?(key)
             hash[key] = value
+            # Debugging: Print the updated hash value
+            # puts "Set default hash[#{key}]: #{hash[key]}"
           end
         end
       end
